8b6e5858bc82c6862d1940bc2e0da4a21657eab1
michaeldrotar <michaeldrotar@gmail.com>
2024-04-13 16:09:53 -0400

    Drop vite project package

--- packages/vite-project/src/math.test.ts
+++
@@ -1,8 +0,0 @@
-import { it, expect } from 'vitest'
-import { add } from './math'
-
-it('must add two integers correctly', () => {
-  const actual = add(10, 25)
-  const expected = 35
-  expect(actual).toEqual(expected)
-})
