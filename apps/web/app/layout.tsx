9de467d0eaed3ad15e8c32b6b0d6cc0c73f82d48
michaeldrotar <michaeldrotar@gmail.com>
2024-04-23 20:30:31 -0400

    Add UserFocusTracker component

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { UserFocusTracker } from '@michaeldrotar/react-focus-tracker/UserFocusTracker'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +17,10 @@ export default function RootLayout({
 }): JSX.Element {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <UserFocusTracker />
+      </body>
     </html>
   )
 }
--- packages/react-focus-tracker/tsup.config.ts
+++ packages/react-focus-tracker/tsup.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig((options) => ({
-  entryPoints: ['src/index.ts', 'src/FocusTrackerRegistration.tsx'],
+  entryPoints: [
+    'src/index.ts',
+    'src/FocusTrackerRegistration.tsx',
+    'src/UserFocusTracker.tsx',
+  ],
   format: ['cjs', 'esm'],
   dts: true,
   sourcemap: true,
