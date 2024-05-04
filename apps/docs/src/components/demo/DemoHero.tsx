b0ade8101123030fc6b65ded62c73e0919b17d9e
michaeldrotar <michaeldrotar@gmail.com>
2024-05-03 23:22:42 -0400

    Move primary/secondary to DemoHero props

--- apps/docs/src/components/demo/DemoHero.tsx
+++ apps/docs/src/components/demo/DemoHero.tsx
@@ -1,20 +1,51 @@
 import { clsx } from 'clsx'
-import type { HTMLAttributes, RefObject } from 'react'
+import type { HTMLAttributes, ReactNode, RefObject } from 'react'
 import styles from './DemoHero.module.css'
+import { DemoBox } from './DemoBox'
 
 export type DemoHeroProps = Pick<
   HTMLAttributes<HTMLElement>,
   'children' | 'className' | 'style'
-> & { rootRef?: RefObject<HTMLDivElement> }
+> & {
+  colored?: boolean
+  rootRef?: RefObject<HTMLDivElement>
+  primaryButton?: ReactNode
+  secondaryButton?: ReactNode
+}
 
 export function DemoHero(props: DemoHeroProps) {
-  const { children, className, rootRef, ...restProps } = props
+  const {
+    children,
+    className,
+    colored,
+    primaryButton,
+    rootRef,
+    secondaryButton,
+    ...restProps
+  } = props
   return (
     <div
-      className={clsx(styles.DemoHero, className)}
+      className={clsx(
+        styles.DemoHero,
+        colored && styles.DemoHeroColored,
+        className,
+      )}
       ref={rootRef}
       {...restProps}
     >
+      <DemoBox
+        style={{
+          position: 'absolute',
+          left: '10%',
+          bottom: '20%',
+          display: 'flex',
+          flexFlow: 'row nowrap',
+          gap: '0.25rem',
+        }}
+      >
+        {primaryButton}
+        {secondaryButton}
+      </DemoBox>
       {children}
     </div>
   )
