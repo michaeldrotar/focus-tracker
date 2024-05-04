d29a7726787d3a62c56434a8631cda814645403a
michaeldrotar <michaeldrotar@gmail.com>
2024-05-03 23:24:36 -0400

    Add DemoWindow actions for clicks/flyouts

--- apps/docs/src/components/demo/DemoWindow.tsx
+++ apps/docs/src/components/demo/DemoWindow.tsx
@@ -5,13 +5,22 @@ import type { FocusTrackerConfiguration } from '@michaeldrotar/focus-tracker-js/
 import { useStableEffect } from '@site/src/hooks/useStableEffect'
 import styles from './DemoWindow.module.css'
 
-type FocusAction = {
-  action: 'focus'
+type BlurAction = {
+  action: 'blur'
+}
+
+type ClickAction = {
+  action: 'click'
   target: string
 }
 
-type BlurAction = {
-  action: 'blur'
+type ClickListItemAction = {
+  action: 'click-item'
+  which: 1 | 2 | 3 | 4 | 5
+}
+
+type CloseListAction = {
+  action: 'close-list'
 }
 
 type ConfigAction = {
@@ -19,7 +28,30 @@ type ConfigAction = {
   config: Partial<FocusTrackerConfiguration>
 }
 
-type AnyAction = FocusAction | BlurAction | ConfigAction
+type FocusAction = {
+  action: 'focus'
+  target: string
+}
+
+type OpenListAction = {
+  action: 'open-list'
+  target: string
+}
+
+type SelectListItemAction = {
+  action: 'select-item'
+  which: 1 | 2 | 3 | 4 | 5
+}
+
+type AnyAction =
+  | BlurAction
+  | ClickAction
+  | ClickListItemAction
+  | CloseListAction
+  | ConfigAction
+  | FocusAction
+  | OpenListAction
+  | SelectListItemAction
 
 export type DemoWindowAction = AnyAction | AnyAction[]
 
@@ -46,7 +78,7 @@ export function DemoWindow(props: DemoWindowProps) {
     centerContent,
     children,
     className,
-    delayInMs,
+    delayInMs = 1000,
     rootRef,
     onInit,
     onAction,
@@ -70,21 +102,109 @@ export function DemoWindow(props: DemoWindowProps) {
     let index = 0
     let timeout: NodeJS.Timeout
     const focusTracker = createOtherFocusTracker()
+    const clickOverlay = element.querySelector('[data-demo-click-overlay]')
+    const flyoutList = element.querySelector('[data-demo-flyout-list]')
+    let flyoutListOpener: HTMLElement | null = null
+
+    const click = (target: HTMLElement, delay: number = delayInMs) => {
+      const position = target.style.position
+      const overflow = target.style.overflow
+      if (!position) target.style.position = 'relative'
+      if (!overflow) target.style.overflow = 'hidden'
+      if (clickOverlay) {
+        target.append(clickOverlay)
+        clickOverlay.classList.add(styles.Clicked)
+        setTimeout(() => {
+          clickOverlay.classList.remove(styles.Clicked)
+          element.append(clickOverlay)
+          target.style.position = position
+          target.style.overflow = overflow
+        }, delay)
+      }
+    }
+
+    const selectItem = (which: 1 | 2 | 3 | 4 | 5) => {
+      element
+        .querySelectorAll('[data-demo-flyout-list] > div')
+        .forEach((item) => {
+          item.classList.remove(styles.Selected)
+        })
+      const item = element.querySelector(`[data-demo-flyout-item-${which}]`)
+      if (item instanceof HTMLElement) {
+        item.classList.add(styles.Selected)
+      }
+    }
+
+    const openList = (target: HTMLElement) => {
+      if (flyoutList instanceof HTMLElement) {
+        selectItem(1)
+        click(target, delayInMs / 2)
+        flyoutListOpener = target
+        setTimeout(() => {
+          focusTracker.focus(flyoutList)
+          flyoutList.classList.add(styles.Opened)
+        }, delayInMs / 2)
+      }
+    }
+
+    const closeList = () => {
+      if (flyoutList instanceof HTMLElement) {
+        flyoutList.classList.remove(styles.Opened)
+        if (flyoutListOpener instanceof HTMLElement) {
+          focusTracker.focus(flyoutListOpener)
+        }
+      }
+    }
+
+    const clickItem = (which: 1 | 2 | 3 | 4 | 5) => {
+      const item = element.querySelector(`[data-demo-flyout-item-${which}]`)
+      if (item instanceof HTMLElement) {
+        click(item, delayInMs / 2)
+        setTimeout(() => {
+          closeList()
+        }, delayInMs / 2)
+      }
+    }
+
+    const getTarget = (target: string) => {
+      const targetElement = element.querySelector(target)
+      if (targetElement instanceof HTMLElement) {
+        return targetElement
+      }
+    }
 
     const runAction = (action: AnyAction) => {
-      let target: Element | null = null
+      const target = 'target' in action ? getTarget(action.target) : undefined
       switch (action.action) {
-        case 'focus':
-          target = element.querySelector(action.target)
-          if (target && target instanceof HTMLElement) {
-            focusTracker.focus(target)
-          }
-          break
         case 'blur':
           focusTracker.blur()
           break
+        case 'click':
+          if (target) {
+            click(target)
+          }
+          break
+        case 'click-item':
+          clickItem(action.which)
+          break
+        case 'close-list':
+          closeList()
+          break
         case 'config':
           focusTracker.configure(action.config)
+          break
+        case 'focus':
+          if (target) {
+            focusTracker.focus(target)
+          }
+          break
+        case 'open-list':
+          if (target) {
+            openList(target)
+          }
+          break
+        case 'select-item':
+          selectItem(action.which)
           break
       }
       if (onActionRef.current) {
@@ -100,7 +220,7 @@ export function DemoWindow(props: DemoWindowProps) {
         runAction(action)
       }
       index = (index + 1) % actions.length
-      timeout = setTimeout(execute, delayInMs || 1000)
+      timeout = setTimeout(execute, delayInMs)
     }
 
     if (onInitRef.current) {
@@ -147,6 +267,32 @@ export function DemoWindow(props: DemoWindowProps) {
           ref={ref}
         >
           {children}
+          <div
+            className={styles.demoWindowClickOverlay}
+            data-demo-click-overlay
+          />
+          <div className={styles.demoWindowFlyoutList} data-demo-flyout-list>
+            <div
+              className={styles.demoWindowFlyoutItem}
+              data-demo-flyout-item-1
+            />
+            <div
+              className={styles.demoWindowFlyoutItem}
+              data-demo-flyout-item-2
+            />
+            <div
+              className={styles.demoWindowFlyoutItem}
+              data-demo-flyout-item-3
+            />
+            <div
+              className={styles.demoWindowFlyoutItem}
+              data-demo-flyout-item-4
+            />
+            <div
+              className={styles.demoWindowFlyoutItem}
+              data-demo-flyout-item-5
+            />
+          </div>
         </div>
       </div>
       <div className={styles.demoWindowShadow} />
