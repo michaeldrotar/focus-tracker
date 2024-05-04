d8f26b930b1ccc3861a5d216bc3feb73de568540
michaeldrotar <michaeldrotar@gmail.com>
2024-05-03 21:28:09 -0400

    Update Docs nav item

--- apps/docs/docusaurus.config.ts
+++ apps/docs/docusaurus.config.ts
@@ -70,7 +70,7 @@ const config: Config = {
           type: 'docSidebar',
           sidebarId: 'tutorialSidebar',
           position: 'right',
-          label: 'Tutorial',
+          label: 'Docs',
         },
         // { to: '/blog', label: 'Blog', position: 'left' },
         {
