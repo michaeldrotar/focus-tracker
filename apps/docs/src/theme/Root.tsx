ab8879d6cc7006402f400b08455017719690a901
michaeldrotar <michaeldrotar@gmail.com>
2024-05-05 15:52:37 -0400

    Add Posthog analytics

--- apps/docs/src/theme/Root.tsx
+++ apps/docs/src/theme/Root.tsx
@@ -1,10 +1,25 @@
 import { FocusTrackerRegistration } from '@michaeldrotar/react-focus-tracker'
 import { UserFocusTracker } from '@michaeldrotar/react-focus-tracker/UserFocusTracker'
-import React from 'react'
+import React, { useEffect } from 'react'
+import { PostHogProvider } from 'posthog-js/react'
+import { useLocation } from '@docusaurus/router'
+import { posthog } from 'posthog-js'
+
+posthog.init('phc_BAB2VZljdFI4ihSLVaIkLumI1d0xoR9tKGIQskpS4SR', {
+  api_host: 'https://us.i.posthog.com',
+  debug: true,
+  capture_pageview: false,
+})
 
 export default function Root({ children }) {
+  const location = useLocation()
+
+  useEffect(() => {
+    posthog.capture('$pageview')
+  }, [location])
+
   return (
-    <>
+    <PostHogProvider client={posthog}>
       <FocusTrackerRegistration
         boxShadow="0 0 0.5rem 1px currentColor"
         color="rgb(var(--theme-color-primary-500))"
@@ -13,6 +28,6 @@ export default function Root({ children }) {
         <div>{children}</div>
       </FocusTrackerRegistration>
       <UserFocusTracker />
-    </>
+    </PostHogProvider>
   )
 }
