9ce681c32a0d11c439d17169ee745cde0cdfc7f1
michaeldrotar <michaeldrotar@gmail.com>
2024-05-05 18:05:51 -0400

    Remove web app

--- apps/web/app/FocusTracker.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-
-import type { FocusTrackerRegistrationProps } from '@michaeldrotar/react-focus-tracker/FocusTrackerRegistration'
-import { FocusTrackerRegistration } from '@michaeldrotar/react-focus-tracker/FocusTrackerRegistration'
-
-export function FocusTracker(props: FocusTrackerRegistrationProps) {
-  return <FocusTrackerRegistration {...props} />
-}
